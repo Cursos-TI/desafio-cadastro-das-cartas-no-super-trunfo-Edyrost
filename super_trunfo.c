0e9e5088740c183a1dc12f2a42476a4553b04cf2
Edyrost <202307160491@alunos.unifavip.edu.br>
2025-08-08 21:45:10 +0000

    commit 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,38 +2,40 @@
     int main() {
     printf("Super Trunfo\n");
 
-    printf("Carta 1: Super Trunfo\n");
-    char estado1[20] = ("São Paulo\n");
-    printf("%s", estado1);
-    char codigo_da_carta1[20] = ("SP1234\n");
-    printf("%s", codigo_da_carta1);
-    char nome_da_cidade1[20] = ("São Paulo\n");
-    printf("%s", nome_da_cidade1);
-    int populacao1 = ("12345000 \n");
-    printf("%d", populacao1);
-    float area1 = ("1521.11\n");
-    printf("%.2f", area1);
-    float pib1 = ("699.28 bilhões de reais\n");
-    printf("%.2f", pib1);
-    int numeros_de_pontos_turisticos1 = ("50 \n");
-    printf("%d", numeros_de_pontos_turisticos1);
+    printf "Carta 1: Super Trunfo";
+    char estado1[20] = "São Paulo";
+    char codigo da cidade1[20] = "SP1234";
+    char nome da cidade1[20] = "São Paulo";
+    int populacao1 = 12345000; 
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int pontos turisticos = 50;
 
+    
+    printf("Estado1 %c"\n", estado1);
+    printf("Nome da Cidade %c \n", nome da cidade1);
+    printf("População %d \n milhões", populacao1);
+    printf("Área %.2f \n km quadrados", area1);
+    printf("PIB %.2f \n, Bilhões de reais");
+    printf("Pontos turísticos %d \n ",pontos turisticos);
 
     printf("Carta 2: Super Trunfo\n");
-    char estado2[20] = ("Rio de Janeiro\n");
-    printf("%s", estado2); 
-     char codigo_da_carta[20] = ("RJ1234\n");
-    printf("%s", codigo_da_carta);
-    char nome_da_cidade[20] = ("Rio de Janeiro\n");
-    printf("%s", nome_da_cidade);
+    char estado2[20] = "Rio de Janeiro";
+    char codigo da cidade[20] = "RJ1234";
+    char nome da cidade[20] = "Rio de Janeiro";
     int populacao2 = 6748000;
-    printf("%d", populacao2);
-    float area2 = 1200.25;
-    printf("%.2f", area2);
+    float area2 = 1200.25; 
     float pib2 = 350.50;
-    printf("%.2f", pib2);
-    int numeros_de_pontos_turisticos2 = 30;
-    printf("%d", numeros_de_pontos_turisticos2);
+    int pontos turisticos  = 30';
+    
+
+    printf("Estado2 %s \c,", estado2); 
+    printf("Nome da Cidade %c", codigo_da_carta);
+    printf("Nome da cidade %d \n", nome da cidade);
+    printf("População2%d", populacao2);
+    printf("Área %.2f \n km quadrados", area2);
+    printf("PIB %.2f \n,bilhões", pib2);
+    printf("Pontos turísticos %d \n", pontosturisticos);
 
     return 0;
 }
