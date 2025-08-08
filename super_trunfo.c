e7ba828a6fe98e92ffafec6a8cadb8f358bb5e89
Edyrost <202307160491@alunos.unifavip.edu.br>
2025-08-08 23:42:24 +0000

    commit iii

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,40 +2,13 @@
     int main() {
     printf("Super Trunfo\n");
 
-    printf "Carta 1: Super Trunfo";
-    char estado1[20] = "São Paulo";
-    char codigo da cidade1[20] = "SP1234";
-    char nome da cidade1[20] = "São Paulo";
-    int populacao1 = 12345000; 
-    float area1 = 1521.11;
-    float pib1 = 699.28;
-    int pontos turisticos = 50;
-
-    
-    printf("Estado1 %c"\n", estado1);
-    printf("Nome da Cidade %c \n", nome da cidade1);
-    printf("População %d \n milhões", populacao1);
-    printf("Área %.2f \n km quadrados", area1);
-    printf("PIB %.2f \n, Bilhões de reais");
-    printf("Pontos turísticos %d \n ",pontos turisticos);
-
-    printf("Carta 2: Super Trunfo\n");
-    char estado2[20] = "Rio de Janeiro";
-    char codigo da cidade[20] = "RJ1234";
-    char nome da cidade[20] = "Rio de Janeiro";
-    int populacao2 = 6748000;
-    float area2 = 1200.25; 
-    float pib2 = 350.50;
-    int pontos turisticos  = 30';
-    
-
-    printf("Estado2 %s \c,", estado2); 
-    printf("Nome da Cidade %c", codigo_da_carta);
-    printf("Nome da cidade %d \n", nome da cidade);
-    printf("População2%d", populacao2);
-    printf("Área %.2f \n km quadrados", area2);
-    printf("PIB %.2f \n,bilhões", pib2);
-    printf("Pontos turísticos %d \n", pontosturisticos);
+    printf "Carta 1\n";
+        char estado: A;
+        printf("Estado %c\n", estado)
+        char codigo[15]: A01;
+        printf("Código da cidade %c\n", codigo)
+        char nome [20]: São Paulo;
+        printf("Nome da cidade %c\n", nome)
 
     return 0;
 }
