bea2fd15996f106bf906cbffb3d3721fb2302b16
Edyrost <202307160491@alunos.unifavip.edu.br>
2025-08-08 20:36:52 +0000

    novo super_trunfo

---
+++ super_trunfo.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+    int main() {
+    printf("Super Trunfo\n");
+
+    printf("Carta 1: Super Trunfo\n");
+    char estado1[20] = ("São Paulo\n");
+    printf("%s", estado1);
+    char codigo_da_carta1[20] = ("SP1234\n");
+    printf("%s", codigo_da_carta1);
+    char nome_da_cidade1[20] = ("São Paulo\n");
+    printf("%s", nome_da_cidade1);
+    int populacao1 = ("12345000 \n");
+    printf("%d", populacao1);
+    float area1 = ("1521.11\n");
+    printf("%.2f", area1);
+    float pib1 = ("699.28 bilhões de reais\n");
+    printf("%.2f", pib1);
+    int numeros_de_pontos_turisticos1 = ("50 \n");
+    printf("%d", numeros_de_pontos_turisticos1);
+
+
+    printf("Carta 2: Super Trunfo\n");
+    char estado2[20] = ("Rio de Janeiro\n");
+    printf("%s", estado2); 
+     char codigo_da_carta[20] = ("RJ1234\n");
+    printf("%s", codigo_da_carta);
+    char nome_da_cidade[20] = ("Rio de Janeiro\n");
+    printf("%s", nome_da_cidade);
+    int populacao2 = 6748000;
+    printf("%d", populacao2);
+    float area2 = 1200.25;
+    printf("%.2f", area2);
+    float pib2 = 350.50;
+    printf("%.2f", pib2);
+    int numeros_de_pontos_turisticos2 = 30;
+    printf("%d", numeros_de_pontos_turisticos2);
+
+    return 0;
+}
