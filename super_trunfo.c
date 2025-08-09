7462c4073f4f2ee2ade25748fe879f8ab341ba24
Edyrost <202307160491@alunos.unifavip.edu.br>
2025-08-09 21:14:41 +0000

    commit rostsim

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
     int main() {
         printf("Carta 1\n");
-        char [1] estado = 'A';
-        char [5] codigo ='A01';
-        char [20] nome = "São Paulo";
-
+       
+        char estado = 'A';
+        char codigo[] = "A01";
+        char nome[20] = "São Paulo";
+        int populacao = 12325000;
+        float area = 1521.11; // Área em km²
+        float pib = 700000.00; // PIB em milhões de reais
+        int pontos = 50;
+        
         printf("Estado 1: %c\n", estado);
         printf("Código 1: %s\n", codigo);
         printf("Nome da Cidade: %s\n", nome);
+        printf("População: %d\n", populacao);
+        printf("Área: %.2f km²\n", area);
+        printf("PIB: %.2f milhões de reais\n", pib);
+        printf("Pontos Turísticos: %d\n", pontos);
+        
 
     return 0;
 }
