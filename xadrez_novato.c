4889067b0a3f7419a87a7d03d1be43fbd43e8bfd
Edyrost <202307160491@alunos.unifavip.edu.br>
2025-10-05 22:17:20 +0000

    xadrez_novatoii

--- xadrez_novato.c
+++ xadrez_novato.c
@@ -11,13 +11,15 @@ int main() {
     printf("Mover Torre 5 casas à Direita\n");
     printf("O Bispo tem que mover 5 casa em diagonal superior à direita\n");
     printf("A Rainha tem que mover 8 casas à direita");
-    
+
     scanf("%d", &movimento);
     for (int i = 0; i < movimento; i++){
         int linha, coluna;
         printf("Move a Torre para a linha (0-5) e coluna (0-0):");
         scanf("%d %d", &linha, &coluna);
         tabuleiro[linha][coluna] = 'T';
+        
+
     }
     
     scanf("%d", &movimento);
@@ -26,6 +28,7 @@ int main() {
         printf("Mover o bispo para a linha (0-7) e coluna (0-7):");
         scanf("%d %d", &linha, &coluna);
         tabuleiro[linha][coluna] = 'B';
+          
     }    
     
     scanf("%d", &movimento);
@@ -34,6 +37,7 @@ int main() {
          printf(" Move a rainha 8 casas à direita");
          scanf("%d %d", &casas, &direita);
          tabuleiro[casas][direita] = 'R';
+        
 
     }     
     
